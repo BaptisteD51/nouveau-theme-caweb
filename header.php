dae58979d10bc8b359299b6fb39a78bfa528284b
Baptiste Dufour <dufourbaptiste08@gmail.com>
2024-01-22 14:17:20 +0100

    forgot to commit a file on last commit

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <p class="site-title"> <?php bloginfo('name'); ?> </p>
         <?php
         wp_nav_menu(['theme_location' => 'main-menu', 'container' => 'nav', 'container_id' => 'main-menu-wrapper', 'menu_id' => 'main-menu',]); //theme_location with underscore !!!
-        wp_nav_menu(['theme_location' => 'social-menu', 'container' => 'nav', 'container_id' => 'social-menu-wrapper', 'menu_id' => 'social-menu',]); //theme_location with underscore !!!
+        wp_nav_menu(['theme_location' => 'social-menu', 'container' => 'nav', 'container_id' => 'social-menu-wrapper', 'menu_id' => 'social-menu',]);
         ?>
         <div id="burger">
             <i class="fa-solid fa-bars"></i>
