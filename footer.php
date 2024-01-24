dfc3d4274a37b447dd0b9a8f61bb4d5a16939801
Baptiste Dufour <dufourbaptiste08@gmail.com>
2024-01-24 17:12:50 +0100

    Forgot to delete some vardump

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
             wp_nav_menu(['theme_location'=>'footer-menu','container' => 'nav','container_id'=>'footer-menu-wrapper','menu_id'=>'footer-menu',]);//theme_location with underscore !!!
             wp_nav_menu(['theme_location' => 'social-menu', 'container' => 'nav', 'container_id' => 'social-menu-wrapper', 'menu_id' => 'social-menu',]);
             wp_nav_menu(['theme_location' => 'contact-menu', 'container' => 'nav', 'container_id' => 'contact-menu-wrapper', 'menu_id' => 'contact-menu',]);
-            var_dump(wp_get_nav_menu_items('contact-menu'));
             get_search_form();
         ?>
     </footer>
